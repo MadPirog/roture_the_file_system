dd83fa88557e3412c90fe70c05d4de122b46de69
MadPirog <fazliev07082009@gmail.com>
2025-05-21 08:55:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,15 @@
 #include "filesystem.h"
 
 int main(){
-  
-
-
+  FILE* fs = open_f("my_fs.dat");
+  create_file(fs, "test.txt", "Hello, world!");
+  char* content = view_f(fs, "test.txt");
+  if (content) {
+    printf("File content:/n%s", content);
+    free(content);
+  }
+fcolse(fs);
+  return 0;
   
 }
 
