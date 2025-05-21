8c5e7ef0f01e522927e8237c4750c559d385d9a7
MadPirog <fazliev07082009@gmail.com>
2025-05-21 09:13:31 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -4,8 +4,8 @@
 FILE *open_f(const char* filename);
 char* view_f(FILE* fs_file, const char* target_filename);
 int delete_f(const char *fs_filename, const char *target_filename);
-int create_file(FILE* fs_file, const char* filename, const char* content);
-int modify_fs(const char* fs_filename, const char* target_filename, const char* new_content);
+int create_f(FILE* fs_file, const char* filename, const char* content);
+int modify_f(const char* fs_filename, const char* target_filename, const char* new_content);
 
 #endif
 
